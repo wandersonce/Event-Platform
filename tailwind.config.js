d6c337a2747af1225b057c087de5ac8fb7db5aca
Wanderson Castro <wandersonce1@hotmail.com>
2022-06-20 16:50:15 -0700

    Setting Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'], //All files inside the src that ends with .tsx will use tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
