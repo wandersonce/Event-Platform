87e7b7777c0ed2b12865e3be6ad943e3e93e2eb2
Wanderson Castro <wandersonce1@hotmail.com>
2022-06-21 13:22:15 -0700

    Using Apollo Hook instead UseEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
 import {useEffect} from 'react';
 import {client} from './lib/appollo';
 
@@ -11,18 +11,20 @@ const GET_LESSONS_QUERY = gql`
   }
 `;
 
+interface Lesson{
+  id: string;
+  title: string;
+}
+
 function App() {
- 
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }).then((response) => {
-      console.log(response.data);
-    });
-  },[]);
+  const {data} = useQuery<{lessons : Lesson[]}>(GET_LESSONS_QUERY);
 
   return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
+    <ul>
+      {data?.lessons.map(lesson =>{
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
