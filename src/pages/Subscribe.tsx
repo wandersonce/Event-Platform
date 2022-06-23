55a4d4554ee7520a6a285b337b7f37398219f796
Wanderson Castro <wandersonce1@hotmail.com>
2022-06-23 15:45:21 -0700

    home structure html

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,8 +1,44 @@
+import { Logo } from "../components/Logo";
 
 export default function Subscribe() {
   return (
     <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
+     
+     <div className='max-w-[1100px] w-full flex justify-between mt-20 mx-auto'>
+      <div className='max-w-[640px]'>
+        <Logo />
+        <h1 className="mt-8 text-[2.5rem] leading-tight">
+          Build a <strong className="text-blue-500">full application</strong>, from scratch, with <strong className="text-blue-500">React </strong>
+        </h1>
+        <p className="mt-4 text-gray-200 leading-relaxed">
+          On only one week you will learn practicing, one of the most popular development stacks!
+        </p>
+      </div>
 
+      <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <strong className="text-2xl mb-6 block"> Subscribe for FREE</strong>
+
+        <form action="" className="flex flex-col gap-2 w-full">
+          <input 
+            className="bg-gray-900 rounded px-5 h-14"
+            type="text" 
+            placeholder="Full Name..."
+            />
+
+          <input 
+            className="bg-gray-900 rounded px-5 h-14"
+            type="email" 
+            placeholder="Type Your Email..." 
+          />
+
+          <button type="submit" className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-small hover:bg-green-700 transition-colors">
+            Hold my Spot!
+          </button>
+        </form>
+      </div>
+     </div>
+
+      <img src="/src/assets/code-mockup.png" alt="Code Screen" className="mt-10" />
     </div>
   )
 }
