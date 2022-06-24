5dc83bf1eb9a92ed0b6cd049ee47d8b2aba3a230
Wanderson Castro <wandersonce1@hotmail.com>
2022-06-23 17:00:14 -0700

    Adding conditional CSS to lessons

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom';
 import {CheckCircle, Lock} from 'phosphor-react';
 import {isPast, format} from 'date-fns';
 
@@ -10,8 +10,12 @@ interface LessonProps{
 }
 
 export default function Lesson(props:LessonProps) {
+  const { slug } = useParams<{ slug: string}>();
+
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormatted = format(props.availableAt, "EEE' • ' d' of ' MMM' • ' K'h'mm");
+
+  const isActiveLesson = slug === props.slug;
 
   return (
     <Link to={`/event/lesson/${props.slug}`} className='group'>
@@ -19,11 +23,11 @@ export default function Lesson(props:LessonProps) {
          {availableDateFormatted}
          </span>
 
-      <div className='rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500'>
+      <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? "bg-green-500" : ""}`}>
         <header className='flex items-center justify-between'>
 
           {isLessonAvailable ? (
-            <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+            <span className={`text-sm font-medium flex items-center gap-2 ${isActiveLesson ? "text-white" : "text-blue-500"}`}>
               <CheckCircle size={20} />
               Content free
             </span>
@@ -41,7 +45,7 @@ export default function Lesson(props:LessonProps) {
 
         </header>
 
-        <strong className='text-gray-200 block mt-5'>
+        <strong className={`mt-5 block ${isActiveLesson ? "text-white" : "text-gray-200"}`}>
           {props.title}
         </strong>
       </div>
